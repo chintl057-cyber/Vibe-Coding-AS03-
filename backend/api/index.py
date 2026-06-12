17f17425a9d9778b812682b153e03712045a8c8f
Matthew Nguyen <nducmanh08@gmail.com>
2026-06-12 17:04:32 +1000

    clean folders/files & backend for deployment 2

--- backend/api/index.py
+++ backend/api/index.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 import sys
 
-BACKEND_DIR = Path(__file__).resolve().parents[1] / "backend"
-if str(BACKEND_DIR) not in sys.path:
-    sys.path.insert(0, str(BACKEND_DIR))
+BACKEND_DIR = Path(__file__).resolve().parents[1]  # THIS IS /backend
 
-from main import app  # noqa: E402
+sys.path.insert(0, str(BACKEND_DIR))
+
+from main import app
